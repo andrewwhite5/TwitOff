5bc92601093da786be39d5a7a8e81c3aefbec9a5
andrewwhite5 <andrewwhite5@users.noreply.github.com>
2020-02-19 00:05:09 -0700

    Adding basilica and tweepy

---
+++ twitoff/basilica_service.py
@@ -0,0 +1,17 @@
+import basilica, os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+basilica_api_key = os.getenv('basilica_api_key', default='Oops')
+
+sentences = [
+    "This is a sentence.",
+    "This is a similar sentence.",
+    "I don't think this sentence is very similar at all..."
+]
+
+with basilica.Connection(basilica_api_key) as c:
+    embeddings = c.embed_sentences(sentences)
+
+print(list(embeddings))
---
+++ twitoff/stocks_service.py
@@ -0,0 +1,15 @@
+import requests, json
+
+request_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=abc123'
+
+response = requests.get(request_url)
+print(response)
+
+print(response.status_code)
+print(type(response.text)) #> string
+
+parsed_response = json.loads(response.text)
+print(type(parsed_response)) #> dict
+print(parsed_response.keys())
+
+# TODO: Work with the parsed responses
